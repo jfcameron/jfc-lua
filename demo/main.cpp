3c450cb8daa33098290a0a0d17a3662b95383722
Joseph Cameron <jf.cameron@gmail.com>
2021-05-18 01:55:15 -0400

    read methods

--- demo/main.cpp
+++ demo/main.cpp
@@ -8,10 +8,22 @@
 #include <memory>
 #include <optional>
 #include <string>
+#include <tuple>
 #include <unordered_map>
 #include <variant>
 
 #include <lua.hpp>
+
+class table
+{
+    using value_type = std::unordered_map<std::string, 
+        std::variant<double, bool, std::string>>;
+
+    using children_type = std::unordered_map<std::string,
+        std::unique_ptr<table>>;
+
+    //...
+};
 
 /// \brief a lua interpreter
 class interpreter final
@@ -32,6 +44,13 @@ public:
     void write_value(const std::string &aPath, const std::string::value_type *aValue);
     /// \brief writes a nil to the lua context
     void write_value(const std::string &aPath, std::nullptr_t);
+
+    /// \brief reads a boolean from the lua context
+    std::optional<bool> read_boolean(const std::string &aPath);
+    /// \brief reads a number from the lua context
+    std::optional<double> read_number(const std::string &aPath);
+    /// \brief reads a string from the lua context
+    std::optional<std::string> read_string(const std::string &aPath);
 
     //TODO: calls a lua function if it exists: use for eg callbacks
     //error try_call_function(path..to..function, paramlist...);
@@ -59,7 +78,7 @@ private:
         m_RegisteredClosures;
 };
 
-static void _write_value(lua_State *L, const std::string &aPathString, std::function<void()> &&aPushValueFunctor)
+static std::tuple<std::vector<std::string>, std::string> _parse_pathstring(const std::string &aPathString)
 {
     static const std::string delimiter(".");
 
@@ -75,6 +94,113 @@ static void _write_value(lua_State *L, const std::string &aPathString, std::func
     }
 
     const std::string variableName = aPathString.substr(last);
+
+    return { path, variableName };
+}
+
+bool _read_value(lua_State *L, const std::string &aPath)
+{
+    auto path_data = _parse_pathstring(aPath);
+
+    std::vector<std::string> &path(std::get<0>(path_data));
+    const std::string &variableName(std::get<1>(path_data));
+
+    if (!path.empty())
+    {
+        lua_getglobal(L, path[0].c_str());
+        if (!lua_istable(L, -1))
+        {
+            lua_pop(L, 1);
+
+            return false;
+        }
+
+        for (size_t i(1); i < path.size(); ++i)
+        {
+            bool nestedTableExists(false);
+
+            lua_pushnil(L);
+            while (lua_next(L, -2))
+            {
+                lua_pushvalue(L, -2);
+
+                if (path[i] == lua_tostring(L, -1) && lua_istable(L, -2))
+                {
+                    nestedTableExists = true;
+
+                    lua_pushvalue(L, -2);
+                    lua_pop(L, 2);
+
+                    break;
+                }
+
+                lua_pop(L, 2);
+            }
+            
+            if (!nestedTableExists)
+            {
+                return false;
+            }
+        }
+    
+        lua_getfield(L, -1, variableName.c_str());
+    }
+    else
+    {
+        lua_getglobal(L, variableName.c_str());
+    }
+   
+    return true;
+}
+
+std::optional<double> interpreter::read_number(const std::string &aPath)
+{
+    auto *L(m_pState.get());
+
+    std::optional<double> val;
+
+    if (_read_value(L, aPath) && lua_isnumber(L, -1)) 
+        val = lua_tonumber(L, -1);
+
+    return val;
+}
+
+std::optional<bool> interpreter::read_boolean(const std::string &aPath)
+{
+    auto *L(m_pState.get());
+
+    std::optional<bool> val;
+
+    if (_read_value(L, aPath) && lua_isboolean(L, -1)) 
+        val = lua_toboolean(L, -1);
+
+    return val;
+}
+
+std::optional<std::string> interpreter::read_string(const std::string &aPath)
+{
+    auto *L(m_pState.get());
+
+    std::optional<std::string> val;
+
+    if (_read_value(L, aPath) && lua_isstring(L, -1))
+    {
+        size_t len;
+        const char *str = lua_tolstring(L, -1, &len);
+
+        std::string msg(str, len);
+        val = msg;
+    }
+
+    return val;
+}
+
+static void _write_value(lua_State *L, const std::string &aPathString, std::function<void()> &&aPushValueFunctor)
+{
+    auto path_data = _parse_pathstring(aPathString);
+
+    std::vector<std::string> &path(std::get<0>(path_data));
+    const std::string &variableName(std::get<1>(path_data));
    
     if (!path.empty())
     {
@@ -118,11 +244,7 @@ static void _write_value(lua_State *L, const std::string &aPathString, std::func
             }
         }
     }
-
-    /*if (auto pVal = std::get_if<bool>(&aValue)) lua_pushboolean(L, static_cast<int>(*pVal));
-    else if (auto pVal = std::get_if<double>(&aValue)) lua_pushnumber(L, *pVal);
-    else if (auto pVal = std::get_if<std::string>(&aValue)) lua_pushstring(L, pVal->c_str());
-    else lua_pushnil(L);*/
+    
     aPushValueFunctor();
 
     if (path.empty()) lua_setglobal(L, variableName.c_str());
@@ -174,19 +296,10 @@ void interpreter::register_function(const std::string &aName,
 
 interpreter::interpreter()
 : m_pState(luaL_newstate(), [](lua_State *p){lua_close(p);})
-{
-    lua_newtable(m_pState.get());
-    lua_setglobal(m_pState.get(), "b");
-
-    lua_getglobal(m_pState.get(), "b");
-    lua_pushstring(m_pState.get(), "blar");
-    lua_pushstring(m_pState.get(), "this is not a test");
-    lua_settable(m_pState.get(), -3);
-}
+{}
 
 interpreter::error_type interpreter::try_run_script(const std::string &aLuaScript) const
 {
-    //TODO: replace with load and run, hash the string, keep loaded around?
     switch (const auto status = luaL_dostring(m_pState.get(), aLuaScript.c_str()))
     {
         case LUA_OK: return {};
@@ -218,7 +331,6 @@ static const std::string init((R"V0G0N(
             goof = "hello"
         }
     }
-
 )V0G0N"));
 
 static const std::string script((R"V0G0N(
@@ -233,7 +345,6 @@ static const std::string script((R"V0G0N(
 int main(int argc, char *argv[])
 {
     interpreter interp;
-    interpreter second_interp;
 
     interp.try_run_script(init);
 
@@ -241,41 +352,41 @@ int main(int argc, char *argv[])
     interp.write_value("debug.written", 456.);
     interp.write_value("debug.a.b.c.d.written", 6545.);
     interp.write_value("global_number", 123.);
-
-    /*int w = 123;
-
-    interp.register_function("b.very_cool", [&w](lua_State *p)
-    {
-        double d = lua_tonumber(p, 1);
-           
-        d = std::sin(d) + w;
-
-        lua_pushnumber(p, d);
-        return 1;
-    });
-
-    interp.register_function("very_silly", [](lua_State *p)
-    {
-        double d = lua_tonumber(p, 1);
-           
-        d -= 123;
-
-        lua_pushnumber(p, d);
-        return 1;
-    });*/
+    interp.write_value("nested.number", 1.);
+    interp.write_value("nes.ted.number", 2.);
     
     interp.register_function("print", [](lua_State *p)
     {
         size_t len;
         const char *str = lua_tolstring(p, 1, &len);
+
+        std::string msg(str, len);
         
-        std::cout << str << "\n";
+        std::cout << msg << "\n";
 
         return 0;
     });
 
     if (auto error = interp.try_run_script(script))
         std::cout << "fist_interp: " << *error << "\n";
+
+    auto val = interp.read_number("global_number");
+    std::cout << "val: " << *val << "\n";
+
+    {
+        auto nestval = interp.read_number("nested.number");
+        std::cout << "nest val: "; 
+        if (nestval.has_value()) std::cout << *nestval;
+        else std::cout << "NOPE";
+        std::cout << "\n";
+    }
+    {
+        auto nestval = interp.read_number("nes.ted.number");
+        std::cout << "nest val: "; 
+        if (nestval.has_value()) std::cout << *nestval;
+        else std::cout << "NOPE";
+        std::cout << "\n";
+    }
 
     return EXIT_SUCCESS;
 }
